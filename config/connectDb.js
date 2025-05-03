842515842d57c675dbec2af373f819df5a21e085
Mayank-Kumar <mayankthakur0711@gmail.com>
2025-05-03 16:58:33 +0530

    commit

--- config/connectDb.js
+++ config/connectDb.js
@@ -3,6 +3,7 @@ const colors = require('colors')
 
 const connectDb = async () => {
     try {
+        mongoose.set('strictQuery', true);
         await mongoose.connect(process.env.MONGO_URL)
         console.log(`Server running on ${mongoose.connection.host}`.bgCyan.white);
     } catch(error) {
